7ee8d3f6223c697fe0f424f0505dda433c1128c0
tyj4188 <617191529@qq.com>
2017-11-06 17:45:40 +0800

    update

--- src/main/java/com/john/Application.java
+++ src/main/java/com/john/Application.java
@@ -11,7 +11,7 @@ package com.john;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author ihui
@@ -19,7 +19,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication // 等同于@Configuration @EnableAutoConfiguration @ComponentScan
 @EnableCaching // 使用缓存
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
