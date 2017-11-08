476804f04367c700777ac50ae6f1a43db85d5a90
tyj4188 <617191529@qq.com>
2017-11-08 17:12:48 +0800

    update

--- src/main/java/com/john/test/controller/GreetingController.java
+++ src/main/java/com/john/test/controller/GreetingController.java
@@ -8,6 +8,7 @@
 
 package com.john.test.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,12 +24,16 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String TEMPLATE = "Hello, %s !";
+    @Value("${server.port}")
+    private String port;
+
+    private static final String TEMPLATE = "Hello, %s ! Hi Im from port %s";
+
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
+        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name, port));
     }
 
     @RequestMapping("/uid")
